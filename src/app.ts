96df6616f508f10b7fc1c88b1cff6b918bd5dcfb
woraphong Somphong <woraphong-s@rmutp.ac.th>
2025-09-07 22:40:01 +0700

    add prisma client and prisma profile

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import { Hono } from "hono";
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello World 555"));
-app.get("/profile", (c) => c.text("Profile"));
+app.get("/", (c) => c.text("Hello World 5555555"));
+app.get("/profile", async (c) => {
+    //get data from db
+    const profiles = await prisma.profile.findMany();
+    //response
+    return c.json({
+        message: "get data complete",
+        data: profiles
+    }, 200);
+});    
+
 export default app;
