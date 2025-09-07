4f650048126b552b05e421cbb270620cfc5572cd
woraphong Somphong <woraphong-s@rmutp.ac.th>
2025-09-07 19:46:40 +0700

    fix src/app.ts for run error

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,6 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello World XXXX!"));
-
+app.get("/", (c) => c.text("Hello World 555"));
+app.get("/profile", (c) => c.text("Profile"));
 export default app;
